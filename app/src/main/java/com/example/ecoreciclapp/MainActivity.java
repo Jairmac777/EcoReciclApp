c75f9662bdf5809402d3a735a3c91044f2cc16f1
Jose Macias <jairmac_7@yahoo.es>
2024-08-01 21:09:43 -0500

    Sexto commit,  corrección y mejora de 6 vistas de la aplicación e interconexión entre las mismas

--- app/src/main/java/com/example/ecoreciclapp/MainActivity.java
+++ app/src/main/java/com/example/ecoreciclapp/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.ecoreciclapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckedTextView;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +18,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText passwordEditText = findViewById(R.id.editTextTextPassword);
+        ImageButton togglePasswordVisibilityOff = findViewById(R.id.togglePasswordVisibilityOff);
+
+        togglePasswordVisibilityOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (passwordEditText.getInputType() == InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD){
+                    passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    togglePasswordVisibilityOff.setImageResource(R.drawable.baseline_visibility_off_24);
+                } else {
+                    passwordEditText.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    togglePasswordVisibilityOff.setImageResource(R.drawable.baseline_visibility_24);
+                }
+                //Mueve el cursor al final del texto
+                passwordEditText.setSelection(passwordEditText.getText().length());
+            }
+        });
+
 
         Button botonIngresa = findViewById(R.id.ingresa);
         CheckedTextView botonRegistrate = findViewById(R.id.registrate);
